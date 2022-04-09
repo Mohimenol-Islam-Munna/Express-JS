3aade962bade039047953f6c137f880b7ac4d22c
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-04-10 00:53:39 +0600

    express middleware

--- WDS/index.js
+++ WDS/index.js
@@ -6,12 +6,15 @@ const studentRoutes = require("./routes/students");
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  //   res.send("hello world");
-  //   res.json({ name: "munna" });
+  // res.send("hello world");
+  // res.json({ name: "munna" });
   console.log("req object :::: ", req.method);
   res.render("index", { name: "Munna", campus: "PUST" });
 });
 
 app.use("/student", studentRoutes);
+app.use((req, res) => {
+  console.log("This function will execute for all incoming request");
+});
 
 app.listen(3000);
